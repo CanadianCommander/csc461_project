566d906ac633348ef23b4adfb08ec58d55a2651a
Lucas Girouard-Stranks <lucas@stranks.ca>
2018-07-03 23:07:56 -0700

    More cleanup

--- src/Program.cpp
+++ src/Program.cpp
@@ -160,11 +160,6 @@ void Program::InitializeOpenGL()
 	glActiveTexture(GL_TEXTURE0);
 	LogGL("glActiveTexture");
 
-	float textureData[] = {
-			1.0f, 1.0f, 0.0f, 0.0f, 1.0f, 0.0f,
-			0.0f, 1.0f, 1.0f, 1.0f, 0.0f, 1.0f
-	};
-
 	std::shared_ptr<IO::Image> img = _screenCapture.GetScreenFrameBuffer();
 
 	_texture = new Graphics::Texture(img->GetWidth(), img->GetHeight(), GL_RGB, GL_RGB, GL_UNSIGNED_BYTE);
@@ -191,80 +186,6 @@ void Program::Frame()
 	Draw();
 }
 
-void PrintEvent(const SDL_Event* event)
-{
-	if (event->type == SDL_WINDOWEVENT)
-	{
-		switch (event->window.event)
-		{
-			case SDL_WINDOWEVENT_SHOWN:
-				SDL_Log("Window %d shown", event->window.windowID);
-				break;
-			case SDL_WINDOWEVENT_HIDDEN:
-				SDL_Log("Window %d hidden", event->window.windowID);
-				break;
-			case SDL_WINDOWEVENT_EXPOSED:
-				SDL_Log("Window %d exposed", event->window.windowID);
-				break;
-			case SDL_WINDOWEVENT_MOVED:
-				SDL_Log("Window %d moved to %d,%d",
-				        event->window.windowID, event->window.data1,
-				        event->window.data2);
-				break;
-			case SDL_WINDOWEVENT_RESIZED:
-				SDL_Log("Window %d resized to %dx%d",
-				        event->window.windowID, event->window.data1,
-				        event->window.data2);
-				glViewport(0, 0, event->window.data1, event->window.data2);
-				break;
-			case SDL_WINDOWEVENT_SIZE_CHANGED:
-				SDL_Log("Window %d size changed to %dx%d",
-				        event->window.windowID, event->window.data1,
-				        event->window.data2);
-				break;
-			case SDL_WINDOWEVENT_MINIMIZED:
-				SDL_Log("Window %d minimized", event->window.windowID);
-				break;
-			case SDL_WINDOWEVENT_MAXIMIZED:
-				SDL_Log("Window %d maximized", event->window.windowID);
-				break;
-			case SDL_WINDOWEVENT_RESTORED:
-				SDL_Log("Window %d restored", event->window.windowID);
-				break;
-			case SDL_WINDOWEVENT_ENTER:
-				SDL_Log("Mouse entered window %d",
-				        event->window.windowID);
-				break;
-			case SDL_WINDOWEVENT_LEAVE:
-				SDL_Log("Mouse left window %d", event->window.windowID);
-				break;
-			case SDL_WINDOWEVENT_FOCUS_GAINED:
-				SDL_Log("Window %d gained keyboard focus",
-				        event->window.windowID);
-				break;
-			case SDL_WINDOWEVENT_FOCUS_LOST:
-				SDL_Log("Window %d lost keyboard focus",
-				        event->window.windowID);
-				break;
-			case SDL_WINDOWEVENT_CLOSE:
-				SDL_Log("Window %d closed", event->window.windowID);
-				break;
-#if SDL_VERSION_ATLEAST(2, 0, 5)
-			case SDL_WINDOWEVENT_TAKE_FOCUS:
-				SDL_Log("Window %d is offered a focus", event->window.windowID);
-				break;
-			case SDL_WINDOWEVENT_HIT_TEST:
-				SDL_Log("Window %d has a special hit test", event->window.windowID);
-				break;
-#endif
-			default:
-				SDL_Log("Window %d got unknown event %d",
-				        event->window.windowID, event->window.event);
-				break;
-		}
-	}
-}
-
 void Program::HandleEvents()
 {
 	SDL_Event e;
@@ -274,8 +195,6 @@ void Program::HandleEvents()
 		{
 			_isExiting = true;
 		}
-
-		PrintEvent(&e);
 	}
 }
 
